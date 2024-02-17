48efa7707b6f6329d878e52b5e2bf3aed1405efc
bokkle <mitch.r_@hotmail.com>
2024-02-17 17:24:12 -0500

    updates

---
+++ src/components/App.js
@@ -0,0 +1,133 @@
+import { useEffect, useReducer } from 'react';
+import Header from './Header';
+import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
+import Question from './Question';
+import NextButton from './NextButton';
+import Progress from './Progress';
+import FinishScreen from './FinishScreen';
+
+const initialState = {
+  questions: [],
+
+  // 'loading, 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    case 'start':
+      return {
+        ...state,
+        status: 'active',
+      };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return {
+        ...state,
+        index: state.index + 1,
+        answer: null,
+      };
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+};
+
+function App() {
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce((acc, cur) => acc + cur.points, 0);
+
+  useEffect(() => {
+    const getQuestions = async () => {
+      try {
+        const res = await fetch(`http://localhost:9000/questions`);
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+      }
+    };
+    getQuestions();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main className="main">
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              numQuestions={numQuestions}
+              index={index}
+            />
+          </>
+        )}
+        {status === 'finished' && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
